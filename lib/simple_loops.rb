6b02a080ad1c37200c721fe0f7ff9481effee451
Nazmul Kabir <nazmul.kabir1216@gmail.com>
2020-05-10 22:04:40 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,13 +24,14 @@ end
 end
 
 def return_string_array(array)
+  new_array
   count=0
   while count<array.length do 
     array[count].to_s 
     count+=1 
     
   end
-   return array
+   new_array 
 end
 
   
